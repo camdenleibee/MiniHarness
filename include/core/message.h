8b3421d2db954a65e00434f54be674e55883f226
Camden Leibee <camdenleibee@gmail.com>
2026-09-21 19:49:43 -0400

    Wrote implementation for Message.h

---
+++ include/core/message.h
@@ -0,0 +1,25 @@
+// include/core/message.h
+#pragma once
+    
+#include <string>
+
+//Role Class which specifies the 
+enum class Role { System, User, Assistant };
+
+class Message {
+public:
+    Message(): role_(Role::System), content_("") {}
+    Message(Role role, std::string content)
+    : role_(role), content_(content){}
+
+    Role role()const noexcept{
+        return role_;
+    }
+    const std::string& content() const noexcept{
+        return content_;
+    }
+
+private:
+    Role role_;
+    std::string content_;
+};
